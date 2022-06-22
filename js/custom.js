8cdd423485b6a73ee668b126426d6ed71a4654e2
Vishal Sondarva <49022855+vishalsondarva@users.noreply.github.com>
2022-06-22 16:39:11 +0530

    new-page

--- js/custom.js
+++ js/custom.js
@@ -86,19 +86,6 @@ if ($(window).innerWidth() <= 991) {
     });
 }
 
-/************************
-    smooth-scroll
-*************************/
-// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-//     anchor.addEventListener('click', function (e) {
-//         e.preventDefault();
-
-//         document.querySelector(this.getAttribute('href')).scrollIntoView({
-//             behavior: 'smooth'
-//         });
-//     });
-// });
-
 
 /************************
     magnificPopup-img
@@ -158,62 +145,64 @@ $(document).ready(function () {
 /************************
     Typing Letter
 *************************/
-var TxtType = function (el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
-};
-
-TxtType.prototype.tick = function () {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
-
-    if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
-
-    this.el.innerHTML = '<label class="wrap">' + this.txt + '</label>';
-
-    var that = this;
-    var delta = 200 - Math.random() * 100;
-
-    if (this.isDeleting) { delta /= 2; }
-
-    if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period;
-        this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
+$(document).ready(function () {
+    var TxtType = function (el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
         this.isDeleting = false;
-        this.loopNum++;
-        delta = 600;
-    }
+    };
 
-    setTimeout(function () {
-        that.tick();
-    }, delta);
-};
+    TxtType.prototype.tick = function () {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
 
-window.onload = function () {
-    var elements = document.getElementsByClassName('typing');
-    for (var i = 0; i < elements.length; i++) {
-        var toRotate = elements[i].getAttribute('data-type');
-        var period = elements[i].getAttribute('data-period');
-        if (toRotate) {
-            new TxtType(elements[i], JSON.parse(toRotate), period);
+        if (this.isDeleting) {
+            this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+            this.txt = fullTxt.substring(0, this.txt.length + 1);
         }
-    }
-    // INJECT CSS
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".typing > .wrap { border-right: 8px solid #d16e51}";
-    document.body.appendChild(css);
-};
+
+        this.el.innerHTML = '<label class="wrap">' + this.txt + '</label>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+            delta = this.period;
+            this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+            this.isDeleting = false;
+            this.loopNum++;
+            delta = 600;
+        }
+
+        setTimeout(function () {
+            that.tick();
+        }, delta);
+    };
+
+    window.onload = function () {
+        var elements = document.getElementsByClassName('typing');
+        for (var i = 0; i < elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+                new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typing > .wrap { border-right: 8px solid #d16e51}";
+        document.body.appendChild(css);
+    };
+});
 
 
 /************************
@@ -227,69 +216,87 @@ $(window).on('load', function () {
 /************************
     Swiper
 *************************/
-var swiper = new Swiper(".slider_wrap", {
-    loop: true,
-    effect: "coverflow",
-    spaceBetween: 0,
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: 3,
-    slideToClickedSlide: true,
-    autoplay: {
-        delay: 3000,
-    },
-    coverflowEffect: {
-        rotate: 0,
-        stretch: 0,
-        depth: 300,
-        modifier: 1,
-        slideShadows: true
-    },
-    breakpoints: {
-        '360': {
-            slidesPerView: 2,
-            coverflowEffect: {
-                rotate: 0,
-                stretch: 0,
-                depth: 200,
-                modifier: 1,
-                slideShadows: true
-            },
+$(document).ready(function () {
+    var swiper = new Swiper(".slider_wrap", {
+        loop: true,
+        effect: "coverflow",
+        spaceBetween: 0,
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 3,
+        slideToClickedSlide: true,
+        autoplay: {
+            delay: 3000,
         },
-        '400': {
-            slidesPerView: 2,
-            coverflowEffect: {
-                rotate: 0,
-                stretch: 0,
-                depth: 300,
-                modifier: 1,
-                slideShadows: true
-            },
+        coverflowEffect: {
+            rotate: 0,
+            stretch: 0,
+            depth: 300,
+            modifier: 1,
+            slideShadows: true
         },
-        '500': {
-            slidesPerView: 3,
-            coverflowEffect: {
-                rotate: 0,
-                stretch: 0,
-                depth: 200,
-                modifier: 1,
-                slideShadows: true
+        breakpoints: {
+            '360': {
+                slidesPerView: 2,
+                coverflowEffect: {
+                    rotate: 0,
+                    stretch: 0,
+                    depth: 200,
+                    modifier: 1,
+                    slideShadows: true
+                },
             },
+            '400': {
+                slidesPerView: 2,
+                coverflowEffect: {
+                    rotate: 0,
+                    stretch: 0,
+                    depth: 300,
+                    modifier: 1,
+                    slideShadows: true
+                },
+            },
+            '500': {
+                slidesPerView: 3,
+                coverflowEffect: {
+                    rotate: 0,
+                    stretch: 0,
+                    depth: 200,
+                    modifier: 1,
+                    slideShadows: true
+                },
+            },
+            '767': {
+                slidesPerView: 3,
+                coverflowEffect: {
+                    rotate: 0,
+                    stretch: 0,
+                    depth: 300,
+                    modifier: 1,
+                    slideShadows: true
+                },
+            }
         },
-        '767': {
-            slidesPerView: 3,
-            coverflowEffect: {
-                rotate: 0,
-                stretch: 0,
-                depth: 300,
-                modifier: 1,
-                slideShadows: true
-            },
+        pagination: {
+            el: ".swiper-pagination"
         }
-    },
-    pagination: {
-        el: ".swiper-pagination"
-    }
+    });
 });
 
+
+
+/************************
+    AOS
+*************************/
+$(document).ready(function () {
+    AOS.init({
+        offset: 80,
+        delay: 100,
+        duration: 1200,
+        easing: 'ease',
+        once: true,
+        mirror: false,
+        anchorPlacement: 'top-bottom',
+    });
+});
 
